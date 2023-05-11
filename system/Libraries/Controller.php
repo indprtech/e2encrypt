62427585b9c0fe0911d5c0df6e26334a08604161
178900PRITAM <pritamkumarsamal@gmail.com>
2023-05-11 23:30:17 +0530

    Make $this->view Deprecated

--- system/Libraries/Controller.php
+++ system/Libraries/Controller.php
@@ -1,8 +1,7 @@
 <?php
 class Controller extends Request{
-
+    /* Type[Deprecated] */
     public function view($location,$parse = []){
         include(__DIR__ . "/../../src/Views/$location.view.php");
     }
-
 }
